0481b9b9f10b7e3e79a02f5b192937cef044dcb4
netbook <netbook@netbook.(none)>
2011-09-25 23:39:09 +0200

    added utillities

---
+++ src/util.h
@@ -0,0 +1,12 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string>
+
+namespace util
+{
+    int stringToInt(const std::string& str);
+    std::string intToString(const int i);
+}
+
+#endif // UTIL_H
