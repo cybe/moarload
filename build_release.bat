9146e4efade59ac8831823d2f0ea51a2f8d64c72
Dennis Keitzel <cybe@arcor.de>
2011-09-27 16:45:34 +0200

    multicore build under windows

--- build_release.bat
+++ build_release.bat
@@ -2,5 +2,5 @@ call "%VS100COMNTOOLS%vsvars32.bat"
 rd build /S /Q
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ..
-nmake
+cmake -G "NMake Makefiles JOM" -DCMAKE_BUILD_TYPE=Release ..
+jom -j2
