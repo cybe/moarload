53c12ee5fea5326b7bb16a8263794704277d7dbd
st0023 <hoe001@freenet.de>
2011-10-05 19:48:29 +0200

    basic framee

--- src/ui/main_frame_view.cpp
+++ src/ui/main_frame_view.cpp
@@ -4,6 +4,9 @@
 //(*InternalHeaders(MainFrameView)
 #include <wx/string.h>
 #include <wx/intl.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/artprov.h>
 //*)
 
 //helper functions
@@ -35,10 +38,24 @@ wxString wxbuildinfo(wxbuildinfoformat format)
 }
 
 //(*IdInit(MainFrameView)
-const long MainFrameView::ID_TEXTCTRL1 = wxNewId();
+const long MainFrameView::ID_PANEL2 = wxNewId();
+const long MainFrameView::ID_PANEL3 = wxNewId();
+const long MainFrameView::ID_PANEL4 = wxNewId();
+const long MainFrameView::ID_PANEL5 = wxNewId();
+const long MainFrameView::ID_MAIN_NOTEBOOK = wxNewId();
+const long MainFrameView::ID_STATICTEXT1 = wxNewId();
+const long MainFrameView::ID_STATICTEXT2 = wxNewId();
+const long MainFrameView::ID_STATICTEXT3 = wxNewId();
+const long MainFrameView::ID_MAIN_STATUS_PANEL = wxNewId();
+const long MainFrameView::ID_MAIN_FRAME_PANEL = wxNewId();
 const long MainFrameView::idMenuQuit = wxNewId();
 const long MainFrameView::idMenuAbout = wxNewId();
-const long MainFrameView::ID_STATUSBAR1 = wxNewId();
+const long MainFrameView::ID_TOOLBARITEM1 = wxNewId();
+const long MainFrameView::ID_TOOLBARITEM3 = wxNewId();
+const long MainFrameView::ID_TOOLBARITEM4 = wxNewId();
+const long MainFrameView::ID_TOOLBARITEM5 = wxNewId();
+const long MainFrameView::ID_TOOLBARITEM6 = wxNewId();
+const long MainFrameView::ID_MAIN_TOOLBAR = wxNewId();
 //*)
 
 BEGIN_EVENT_TABLE(MainFrameView, wxFrame)
@@ -51,34 +68,67 @@ MainFrameView::MainFrameView(wxWindow* parent, wxWindowID id)
 
     //(*Initialize(MainFrameView)
     wxMenuItem* MenuItem2;
+    wxBoxSizer* mainFrameSizer;
     wxMenuItem* MenuItem1;
     wxMenu* Menu1;
-    wxMenuBar* MenuBar1;
+    wxBoxSizer* mainStatusPanelSizer;
+    wxMenuBar* mainMenuBar;
     wxMenu* Menu2;
-
-    Create(parent, id, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("id"));
-    SetClientSize(wxSize(318, 260));
-    TextCtrl1 = new wxTextCtrl(this, ID_TEXTCTRL1, _("foobar"), wxPoint(128, 144), wxDefaultSize, wxTE_MULTILINE, wxDefaultValidator, _T("ID_TEXTCTRL1"));
-    MenuBar1 = new wxMenuBar();
+    
+    Create(parent, wxID_ANY, _("Moarload"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("wxID_ANY"));
+    SetClientSize(wxSize(318,260));
+    mainFramePanel = new wxPanel(this, ID_MAIN_FRAME_PANEL, wxPoint(80,56), wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_MAIN_FRAME_PANEL"));
+    mainFrameSizer = new wxBoxSizer(wxVERTICAL);
+    mainNotebook = new wxNotebook(mainFramePanel, ID_MAIN_NOTEBOOK, wxDefaultPosition, wxDefaultSize, 0, _T("ID_MAIN_NOTEBOOK"));
+    downloadPanel = new wxPanel(mainNotebook, ID_PANEL2, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL2"));
+    addPanel = new wxPanel(mainNotebook, ID_PANEL3, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL3"));
+    settingsPanel = new wxPanel(mainNotebook, ID_PANEL4, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL4"));
+    logPanel = new wxPanel(mainNotebook, ID_PANEL5, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL5"));
+    mainNotebook->AddPage(downloadPanel, _("Download"), false);
+    mainNotebook->AddPage(addPanel, _("Add"), false);
+    mainNotebook->AddPage(settingsPanel, _("Settings"), false);
+    mainNotebook->AddPage(logPanel, _("Log"), false);
+    mainFrameSizer->Add(mainNotebook, 1, wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    mainStatusPanel = new wxPanel(mainFramePanel, ID_MAIN_STATUS_PANEL, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_MAIN_STATUS_PANEL"));
+    mainStatusPanel->SetMinSize(wxSize(-1,24));
+    mainStatusPanel->SetMaxSize(wxSize(-1,24));
+    mainStatusPanelSizer = new wxBoxSizer(wxHORIZONTAL);
+    StaticText1 = new wxStaticText(mainStatusPanel, ID_STATICTEXT1, _("Label"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT1"));
+    mainStatusPanelSizer->Add(StaticText1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    StaticText2 = new wxStaticText(mainStatusPanel, ID_STATICTEXT2, _("Label"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT2"));
+    mainStatusPanelSizer->Add(StaticText2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    StaticText3 = new wxStaticText(mainStatusPanel, ID_STATICTEXT3, _("Label"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT3"));
+    mainStatusPanelSizer->Add(StaticText3, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    mainStatusPanel->SetSizer(mainStatusPanelSizer);
+    mainStatusPanelSizer->Fit(mainStatusPanel);
+    mainStatusPanelSizer->SetSizeHints(mainStatusPanel);
+    mainFrameSizer->Add(mainStatusPanel, 0, wxEXPAND|wxALIGN_BOTTOM|wxALIGN_CENTER_HORIZONTAL, 0);
+    mainFramePanel->SetSizer(mainFrameSizer);
+    mainFrameSizer->Fit(mainFramePanel);
+    mainFrameSizer->SetSizeHints(mainFramePanel);
+    mainMenuBar = new wxMenuBar();
     Menu1 = new wxMenu();
     MenuItem1 = new wxMenuItem(Menu1, idMenuQuit, _("Quit\tAlt-F4"), _("Quit the application"), wxITEM_NORMAL);
     Menu1->Append(MenuItem1);
-    MenuBar1->Append(Menu1, _("&File"));
+    mainMenuBar->Append(Menu1, _("&File"));
     Menu2 = new wxMenu();
     MenuItem2 = new wxMenuItem(Menu2, idMenuAbout, _("About\tF1"), _("Show info about this application"), wxITEM_NORMAL);
     Menu2->Append(MenuItem2);
-    MenuBar1->Append(Menu2, _("Help"));
-    SetMenuBar(MenuBar1);
-    StatusBar1 = new wxStatusBar(this, ID_STATUSBAR1, 0, _T("ID_STATUSBAR1"));
-    int __wxStatusBarWidths_1[1] = { -1 };
-    int __wxStatusBarStyles_1[1] = { wxSB_NORMAL };
-    StatusBar1->SetFieldsCount(1, __wxStatusBarWidths_1);
-    StatusBar1->SetStatusStyles(1, __wxStatusBarStyles_1);
-    SetStatusBar(StatusBar1);
-
-    Connect(ID_TEXTCTRL1, wxEVT_COMMAND_TEXT_UPDATED, (wxObjectEventFunction)&MainFrameView::OnTextCtrl1Text);
-    Connect(idMenuQuit, wxEVT_COMMAND_MENU_SELECTED, (wxObjectEventFunction)&MainFrameView::OnQuit);
-    Connect(idMenuAbout, wxEVT_COMMAND_MENU_SELECTED, (wxObjectEventFunction)&MainFrameView::OnAbout);
+    mainMenuBar->Append(Menu2, _("Help"));
+    SetMenuBar(mainMenuBar);
+    mainToolBar = new wxToolBar(this, ID_MAIN_TOOLBAR, wxDefaultPosition, wxDefaultSize, wxTB_HORIZONTAL|wxTB_TEXT|wxNO_BORDER, _T("ID_MAIN_TOOLBAR"));
+    ToolBarItem1 = mainToolBar->AddTool(ID_TOOLBARITEM1, _("Start/Stop"), wxArtProvider::GetBitmap(wxART_MAKE_ART_ID_FROM_STR(_T("wxART_GO_FORWARD")),wxART_TOOLBAR), wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString);
+    mainToolBar->AddSeparator();
+    ToolBarItem2 = mainToolBar->AddTool(ID_TOOLBARITEM3, _("Stop Next"), wxArtProvider::GetBitmap(wxART_MAKE_ART_ID_FROM_STR(_T("wxART_GO_DOWN")),wxART_TOOLBAR), wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString);
+    ToolBarItem3 = mainToolBar->AddTool(ID_TOOLBARITEM4, _("Auto Reconnect"), wxArtProvider::GetBitmap(wxART_MAKE_ART_ID_FROM_STR(_T("wxART_ERROR")),wxART_TOOLBAR), wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString);
+    ToolBarItem4 = mainToolBar->AddTool(ID_TOOLBARITEM5, _("Auto Clipboard"), wxArtProvider::GetBitmap(wxART_MAKE_ART_ID_FROM_STR(_T("wxART_PASTE")),wxART_TOOLBAR), wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString);
+    mainToolBar->AddSeparator();
+    ToolBarItem5 = mainToolBar->AddTool(ID_TOOLBARITEM6, _("Reconnect Now"), wxArtProvider::GetBitmap(wxART_MAKE_ART_ID_FROM_STR(_T("wxART_ERROR")),wxART_TOOLBAR), wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString);
+    mainToolBar->Realize();
+    SetToolBar(mainToolBar);
+    
+    Connect(idMenuQuit,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&MainFrameView::OnQuit);
+    Connect(idMenuAbout,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&MainFrameView::OnAbout);
     //*)
 }
 
--- src/ui/main_frame_view.h
+++ src/ui/main_frame_view.h
@@ -2,10 +2,13 @@
 #define MAIN_FRAME_VIEW_H
 
 //(*Headers(MainFrameView)
+#include <wx/toolbar.h>
+#include <wx/sizer.h>
+#include <wx/notebook.h>
 #include <wx/menu.h>
-#include <wx/statusbr.h>
+#include <wx/panel.h>
 #include <wx/frame.h>
-#include <wx/textctrl.h>
+#include <wx/stattext.h>
 //*)
 
 class MainFrameView: public wxFrame
@@ -21,18 +24,47 @@ private:
     void OnQuit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
     void OnTextCtrl1Text(wxCommandEvent& event);
+    void OnNotebook1PageChanged(wxNotebookEvent& event);
     //*)
 
     //(*Identifiers(MainFrameView)
-    static const long ID_TEXTCTRL1;
+    static const long ID_PANEL2;
+    static const long ID_PANEL3;
+    static const long ID_PANEL4;
+    static const long ID_PANEL5;
+    static const long ID_MAIN_NOTEBOOK;
+    static const long ID_STATICTEXT1;
+    static const long ID_STATICTEXT2;
+    static const long ID_STATICTEXT3;
+    static const long ID_MAIN_STATUS_PANEL;
+    static const long ID_MAIN_FRAME_PANEL;
     static const long idMenuQuit;
     static const long idMenuAbout;
-    static const long ID_STATUSBAR1;
+    static const long ID_TOOLBARITEM1;
+    static const long ID_TOOLBARITEM3;
+    static const long ID_TOOLBARITEM4;
+    static const long ID_TOOLBARITEM5;
+    static const long ID_TOOLBARITEM6;
+    static const long ID_MAIN_TOOLBAR;
     //*)
 
     //(*Declarations(MainFrameView)
-    wxStatusBar* StatusBar1;
-    wxTextCtrl* TextCtrl1;
+    wxToolBarToolBase* ToolBarItem5;
+    wxPanel* settingsPanel;
+    wxPanel* logPanel;
+    wxPanel* downloadPanel;
+    wxStaticText* StaticText1;
+    wxToolBarToolBase* ToolBarItem2;
+    wxStaticText* StaticText3;
+    wxPanel* mainFramePanel;
+    wxPanel* addPanel;
+    wxToolBarToolBase* ToolBarItem4;
+    wxToolBar* mainToolBar;
+    wxToolBarToolBase* ToolBarItem1;
+    wxStaticText* StaticText2;
+    wxToolBarToolBase* ToolBarItem3;
+    wxPanel* mainStatusPanel;
+    wxNotebook* mainNotebook;
     //*)
 
     DECLARE_EVENT_TABLE()
