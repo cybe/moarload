b055202942b51c82a1ff739af1f5a3b63b148c93
Dennis Keitzel <cybe@arcor.de>
2011-10-10 05:18:28 +0200

    cleaned commented out code

--- src/ui/model/download_list_model_node.h
+++ src/ui/model/download_list_model_node.h
@@ -104,6 +104,8 @@ private:
     std::vector<DownloadListModelNode*> m_children;
 };
 
+
+
 class DownloadListModelNodeBackend : public DownloadListModelNode
 {
 public:
@@ -201,6 +203,7 @@ private:
     DownloadListModelNodeBackend(const DownloadListModelNodeBackend&);
     void operator=(const DownloadListModelNodeBackend&);
 };
+
 
 
 class DownloadListModelNodePackage : public DownloadListModelNode
@@ -303,6 +306,8 @@ private:
     PackageData m_packageData;
 };
 
+
+
 class DownloadListModelNodeFile : public DownloadListModelNode
 {
 public:
@@ -388,147 +393,5 @@ private:
 
     FileData m_fileData;
 };
-
-//class DownloadListModelNode
-//{
-//
-//private:
-//    DownloadListModelNode(const DownloadListModelNode& rhs);
-//    DownloadListModelNode& operator=(const DownloadListModelNode& rhs);
-//
-//    DownloadListModelNode* m_parentNode;
-//    DownloadListModelNodePtrArray m_childrenNodes;
-//    short                   m_order;
-//    wxString                m_name;
-//    wxString                m_hoster;
-//    Priority                m_priority;
-//    wxString                m_status;
-//    wxString                m_progressText;
-//    int                     m_progress;
-//    bool                    m_container;
-//
-//public:
-//    DownloadListModelNode(DownloadListModelNode* parent, const wxString& backendName) {
-//        m_order = -1;
-//        m_priority = -1;
-//        m_progress = -1;
-//        m_parentNode = parent;
-//        m_name = backendName;
-//        m_container = true;
-//    }
-//
-//    DownloadListModelNode(DownloadListModelNode* parent,
-//                          int order,
-//                          const wxString& name,
-//                          const wxString& hoster,
-//                          int priority,
-//                          const wxString& status,
-//                          const wxString& progressText,
-//                          int progress,
-//                          bool container = false) {
-//
-//        m_parentNode = parent;
-//        m_order = order;
-//        m_name = name;
-//        m_hoster = hoster;
-//        m_priority = priority;
-//        m_status = status;
-//        m_progressText = progressText;
-//        m_progress = progress;
-//        m_container = container;
-//    }
-//
-//    virtual ~DownloadListModelNode() {
-//        size_t count = m_childrenNodes.GetCount();
-//        for (size_t i = 0; i < count; i++) {
-//            DownloadListModelNode* child = m_childrenNodes[i];
-//            delete child;
-//        }
-//    }
-//
-//    bool IsContainer() const {
-//        return m_container;
-//    }
-//
-//    DownloadListModelNode* GetParent() {
-//        return m_parentNode;
-//    }
-//    DownloadListModelNodePtrArray& GetChildren() {
-//        return m_childrenNodes;
-//    }
-//    DownloadListModelNode* GetNthChild(unsigned int n) {
-//        return m_childrenNodes.Item(n);
-//    }
-//    void Insert(DownloadListModelNode* child, unsigned int n) {
-//        m_childrenNodes.Insert(child, n);
-//    }
-//    void Append(DownloadListModelNode* child) {
-//        m_childrenNodes.Add(child);
-//    }
-//    unsigned int GetChildCount() const {
-//        return m_childrenNodes.GetCount();
-//    }
-//
-//    void setChildrenNodes(const DownloadListModelNodePtrArray& childrenNodes) {
-//        this->m_childrenNodes = childrenNodes;
-//    }
-//    void setContainer(bool container) {
-//        this->m_container = container;
-//    }
-//    void setHoster(const wxString& hoster) {
-//        this->m_hoster = hoster;
-//    }
-//    void setName(const wxString& name) {
-//        this->m_name = name;
-//    }
-//    void setOrder(short order) {
-//        this->m_order = order;
-//    }
-//    void setParentNode(DownloadListModelNode* parentNode) {
-//        this->m_parentNode = parentNode;
-//    }
-//    void setPriority(const Priority& priority) {
-//        this->m_priority = priority;
-//    }
-//    void setProgress(int progress) {
-//        this->m_progress = progress;
-//    }
-//    void setProgressText(const wxString& progressText) {
-//        this->m_progressText = progressText;
-//    }
-//    void setStatus(const wxString& status) {
-//        this->m_status = status;
-//    }
-//    const DownloadListModelNodePtrArray& getChildrenNodes() const {
-//        return m_childrenNodes;
-//    }
-//    bool getContainer() const {
-//        return m_container;
-//    }
-//    const wxString& getHoster() const {
-//        return m_hoster;
-//    }
-//    const wxString& getName() const {
-//        return m_name;
-//    }
-//    short getOrder() const {
-//        return m_order;
-//    }
-//    DownloadListModelNode* getParentNode() {
-//        return m_parentNode;
-//    }
-//    const Priority& getPriority() const {
-//        return m_priority;
-//    }
-//    int getProgress() const {
-//        return m_progress;
-//    }
-//    const wxString& getProgressText() const {
-//        return m_progressText;
-//    }
-//    const wxString& getStatus() const {
-//        return m_status;
-//    }
-//};
 
 #endif // DOWNLOAD_LIST_MODEL_NODE_H
